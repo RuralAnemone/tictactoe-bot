290e61a332571206c38d67706b10584ea9544f2d
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-03-17 18:02:26 -0500

    2 player mode is now functional yay nice etc

--- Main.java
+++ Main.java
@@ -1,22 +1,34 @@
 import java.util.Scanner;
 
-public class Main
+public class TicTacToeTester
 {
     public static void main(String[] args)
     {
         TicTacToe game = new TicTacToe();
 
         // setup
-        int gameType = 3;
-        while (gameType <=1 && gameType >= 0) {
-            gameType = Integer.parseInt(prompt("0: Player against Player\n1: Player against Bot\n\nenter game type: "));
+        int gameType = 7;
+        while (gameType != 1 && gameType != 0) {
+            gameType = Integer.parseInt(prompt("0: Player vs Player\n1: Player vs Bot\n\nenter game type: "));
         }
-        
-        
 
         for (int i = 0; i < 9; i++) {
-            twoPlayerGame(game);
+            if (game.checkWin()) continue;
+
+            if (gameType == 0) twoPlayerGame(game);
+            else onePlayerGame(game);
+            
         }
+
+        clear();
+        game.printBoard();
+        System.out.println();
+
+        if (game.checkWin()) {
+            System.out.println("Game over!\n");
+            System.out.println((game.getTurn() % 2 == 1 ? "X" : "O") + " won.");
+        } else System.out.println("It's a tie! :(");
+
     }
 
     static void twoPlayerGame(TicTacToe game) {
@@ -28,12 +40,15 @@ public class Main
         int col = 69;
         while (!game.pickLocation(row, col)) {
             System.out.println("⚠ this program will cry if you don't enter an integer between 0 and 2 ⚠\n");
+            System.out.println("It's " + (game.getTurn() % 2 == 0 ? "X" : "O") + "'s turn");
             System.out.println("make a move:");
             row = Integer.parseInt(prompt("row: "));
             col = Integer.parseInt(prompt("col: "));
         }
         game.takeTurn(row, col);
     }
+
+    static void onePlayerGame(TicTacToe game) { }
 
     static String prompt(String text) {
         Scanner input = new Scanner(System.in);
@@ -42,6 +57,6 @@ public class Main
     }
 
     static void clear() {
-        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     }
 }
---
+++ Zobrist.java
@@ -0,0 +1,5 @@
+public class Zobrist {
+    public static void main(String[] args) {
+        
+    }
+}
