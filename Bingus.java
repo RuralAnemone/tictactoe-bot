c0e9bf18d8deb6c4138797f23a856b980ca94646
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-03-20 09:13:19 -0500

    savestate

---
+++ Bingus.java
@@ -0,0 +1,3 @@
+public class Bingus {
+    public Bingus() { }
+}
--- Zobrist.java
+++ Zobrist.java
@@ -1,12 +1,42 @@
-// you guess whether I wrote this or whether it was gpt-3
+// // you guess whether I wrote this first part or whether it was gpt-3
 
 public class Zobrist {
 
-    private long[][] zobristTable;
+    // private long[][] zobristTable;
     private int size;
+    private String[][] board;
+    private String[] digits = {
+        "0",
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+        "9",
+        "a",
+        "b",
+        "c",
+        "d",
+        "e",
+        "f",
+        "g",
+        "h",
+        "i",
+        "j",
+        "k",
+        "l",
+        "m",
+        "n",
+        "o",
+        "p",
+        "q"
+    };
 
     // constructor
-    public Zobrist(TicTacToe board) {
+    /* public Zobrist(TicTacToe board) {
         this.size = board.getBoard().length;
         zobristTable = new long[size][size];
         for (int i = 0; i < size; i++) {
@@ -14,6 +44,9 @@ public class Zobrist {
                 zobristTable[i][j] = generateRandomNumber();
             }
         }
+    } */
+    public Zobrist(TicTacToe board) {
+        this.board = board.getBoard(); // hehe wow that's fun to read :)
     }
     
     // generate random numbers for zobrist table
@@ -22,7 +55,7 @@ public class Zobrist {
     }
 
     // generate a hash value for the tic-tac-toe board
-    public long hash(TicTacToe board) {
+    public long zHash(TicTacToe board) {
         long hash = 0;
         String[][] boardState = board.getBoard();
         for (int i = 0; i < size; i++) {
@@ -35,6 +68,26 @@ public class Zobrist {
             }
         }
         return hash;
+    }
+
+    // ok so the board hash is a ternary number (troll emoji) where 0 is empty space, 1 is X, and 2 is O
+    // ... and then it's condensed into nonary and then trinonary
+    public String /* for minimal data loss??? */ hash(TicTacToe board) { //you know, I should really make my own data type for this :)
+        String hash = "";
+        for (String[] e : board) {
+            for (String f : e) {
+                if (f.equals("-")) hash += "0";
+                else if (f.equals("X")) hash += "1";
+                else if (f.equals("O")) hash += "2";
+                else throw new Bingus();
+            }
+        }
+        return condense(hash);
+    }
+
+    public String condense(String ternaryString) {
+        // to trinonary (base-27, [0-9a-q])!!
+        
     }
 
 }
