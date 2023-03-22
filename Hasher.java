a0f8093d817c0c6556e66d89bc221825de0c6aea
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-03-22 12:13:01 -0500

    nah no more zobrist

--- Hasher.java
+++ Hasher.java
@@ -1,5 +1,3 @@
-// // you guess whether I wrote this first part or whether it was gpt-3
-
 public class Zobrist {
 
 	// private long[][] zobristTable;
@@ -7,39 +5,9 @@ public class Zobrist {
 	private String[][] board;
 
 	// constructor
-	/* public Zobrist(TicTacToe board) {
-		this.size = board.getBoard().length;
-		zobristTable = new long[size][size];
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				zobristTable[i][j] = generateRandomNumber();
-			}
-		}
-	} */
-	public Zobrist(TicTacToe board) {
+	public Hasher(TicTacToe board) {
 		this.board = board.getBoard(); // hehe wow that's fun to read :)
 		// board board board board board board board board board board board
-	}
-		
-	// generate random numbers for zobrist table
-	private long generateRandomNumber() {
-		return (long) (Math.random() * Long.MAX_VALUE);
-	}
-
-	// generate a hash value for the tic-tac-toe board
-	public long zHash(TicTacToe board) {
-		long hash = 0;
-		String[][] boardState = board.getBoard();
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				if (boardState[i][j] == "X") {
-					hash ^= zobristTable[i][j];
-				} else if (boardState[i][j] == "O") {
-					hash ^= zobristTable[i][j];
-				}
-			}
-		}
-		return hash;
 	}
 
 	// ok so the board hash is a ternary number (troll emoji) where 0 is empty space, 1 is X, and 2 is O
@@ -61,7 +29,5 @@ public class Zobrist {
 		// to trinonary (base-27, [0-9a-q])!!
 		return Integer.toString(Integer.parseInt(ternaryString, 3), 27);
 	}
-
-	// come to think of it my comment placements are hardly standardized at all
 
 }
--- ZobristTester.java
+++
@@ -1,6 +0,0 @@
-public class ZobristTester {
-	public static void main(String[] args) {
-		TicTacToe board = new TicTacToe();
-		Zobrist hash = new Zobrist
-	}
-}
