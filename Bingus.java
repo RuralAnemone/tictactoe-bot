982d4452c48da32be2fbccdb615c2707ad294084
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-03-23 10:57:19 -0500

    oopsie :)

--- Bingus.java
+++ Bingus.java
@@ -1,6 +1,6 @@
 public class Bingus extends Exception {
 	public Bingus() { super(); }
 	public Bingus(String message) { super(message); }
-    public Bingus(String message, Throwable cause) { super(message, cause); }
-    public Bingus(Throwable cause) { super(cause); }
+	public Bingus(String message, Throwable cause) { super(message, cause); }
+	public Bingus(Throwable cause) { super(cause); }
 }
