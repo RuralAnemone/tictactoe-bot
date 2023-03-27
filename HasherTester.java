e43b9346f75f7c404f202365bcadd3db91fba0b0
Rural Anemone <64103005+RuralAnemone@users.noreply.github.com>
2023-03-27 14:16:27 -0500

    me when savestate

--- HasherTester.java
+++ HasherTester.java
@@ -1,23 +1,20 @@
 public class HasherTester {
-    
+
 	static TicTacToe board = new TicTacToe();
-    
+	static int[][] moves = {
+
+	}
+
 	public static void main(String[] args) {
-		moves();
+		playMoves();
 
 		System.out.println(Hasher.hash(board));
 
 	}
 
-	static void moves() {
-		board.takeTurn(2,2);
-		board.takeTurn(1,1);
-		board.takeTurn(0,1);
-		board.takeTurn(1,2);
-		board.takeTurn(0,2);
-		board.takeTurn(0,0);
-		board.takeTurn(1,0);
-		board.takeTurn(2,0);
-		board.takeTurn(2,1);
+	static void playMoves() {
+		for (int[] e : moves) {
+			board.takeTurn(e[0], e[1]);
+		}
 	}
 }
